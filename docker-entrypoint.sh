6a40f1e44feda78c35efe03b159fd508fae98504
mvuser <mvuser@mvmb114.lab.moviri.com>
2017-07-06 10:15:19 +0200

    Timeout on client execution

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,15 @@ curl $1:8082/clear_cache
 
 sleep 10
 
-/ycsb/bin/ycsb run cassandra-cql -p hosts=$1 -jvm-args "-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=7199 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Xmx4G -Xms4G -XX:+AlwaysPreTouch"  $YCSB_RUN
+#bad parameters can lead to painfully slow servers, set a timeout
+opcount=`echo $YCSB_RUN | sed -r 's/.*recordcount=([0-9]+).*/\1/g'`
+tgt=`echo $YCSB_RUN | sed -r 's/.*target ([0-9]+).*/\1/g'`
+limit=$(python -c "print int(2*$opcount / $tgt)")
 
+timeout $limit /ycsb/bin/ycsb run cassandra-cql -p hosts=$1 -jvm-args "-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=7199 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false "  $YCSB_RUN
+
+if [ $? -eq 124 ]; then
+	exit 124
+fi
 #we should add -p cassandra.writeconsistencylevel=QUORUM -p cassandcra.readconsistencylevel=QUORUM
 #when we will use more than one server
