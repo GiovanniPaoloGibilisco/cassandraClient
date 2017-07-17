5a948aa8e925fd0624327fe78a990f7798f2a7fd
mvuser <mvuser@mvmb114.lab.moviri.com>
2017-07-17 11:39:59 +0200

    fail faster on bad parameters

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,7 +13,7 @@ op=`echo $YCSB_RUN | sed -r 's/.*operationcount=([0-9]+).*/\1/g'`
 tg=`echo $YCSB_RUN | sed -r 's/.*target ([0-9]+).*/\1/g'`
 echo $op
 echo $tg
-lim=$(python -c "print int(2*$op / $tg)")
+lim=$(python -c "print int(1.15*$op / $tg)")
 echo $lim
 
 timeout $lim /ycsb/bin/ycsb run cassandra-cql -p hosts=$1 -jvm-args "-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=7199 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false "  $YCSB_RUN
